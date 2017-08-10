e66f2069660ec129d01e200378a8a12a18e6753f
teprinciple <teprinciple@foxmail.com>
2017-08-10 23:27:33 +0800

    成功提交

---
+++ UpdateAppUtils/src/main/java/teprinciple/library/feature/Callback.java
@@ -0,0 +1,8 @@
+package teprinciple.library.feature;
+
+/**
+ * Created by sanmu on 2016/10/13 0013.
+ */
+public interface Callback {
+    public void callback();
+}
---
+++ UpdateAppUtils/src/main/java/teprinciple/library/util/DownloadAppUtils.java
@@ -0,0 +1,81 @@
+package teprinciple.library.util;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.File;
+
+
+/**
+ *Created by Teprinciple on 2016/12/13.
+ */
+public class DownloadAppUtils {
+    private static final String TAG = DownloadAppUtils.class.getSimpleName();
+    public static long downloadUpdateApkId = -1;//下载更新Apk 下载任务对应的Id
+    public static String downloadUpdateApkFilePath;//下载更新Apk 文件路径
+
+    /**
+     * 通过浏览器下载APK包
+     * @param context
+     * @param url
+     */
+    public static void downloadForWebView(Context context, String url) {
+        Uri uri = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+
+    /**
+     * 下载更新apk包
+     * 权限:1,<uses-permission android:name="android.permission.DOWNLOAD_WITHOUT_NOTIFICATION" />
+     * @param context
+     * @param url
+     */
+    public static void downloadForAutoInstall(Context context, String url, String fileName, String title) {
+        //LogUtil.e("App 下载 url="+url+",fileName="+fileName+",title="+title);
+        if (TextUtils.isEmpty(url)) {
+            return;
+        }
+        try {
+            Uri uri = Uri.parse(url);
+            DownloadManager downloadManager = (DownloadManager) context
+                    .getSystemService(Context.DOWNLOAD_SERVICE);
+            DownloadManager.Request request = new DownloadManager.Request(uri);
+            //在通知栏中显示
+            request.setVisibleInDownloadsUi(true);
+            request.setTitle(title);
+            String filePath = null;
+            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {//外部存储卡
+                filePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+            } else {
+                Log.i(TAG,"没有SD卡");
+                return;
+            }
+            downloadUpdateApkFilePath = filePath + File.separator + fileName;
+            // 若存在，则删除
+            deleteFile(downloadUpdateApkFilePath);
+            Uri fileUri = Uri.parse("file://" + downloadUpdateApkFilePath);
+            request.setDestinationUri(fileUri);
+            downloadUpdateApkId = downloadManager.enqueue(request);
+        } catch (Exception e) {
+            e.printStackTrace();
+            downloadForWebView(context, url);
+        }finally {
+//            registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+        }
+    }
+
+
+    private static boolean deleteFile(String fileStr) {
+        File file = new File(fileStr);
+        return file.delete();
+    }
+}
