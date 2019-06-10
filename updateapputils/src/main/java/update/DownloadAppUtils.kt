f58ee9fc819e441a3e2df0e4f26a542e45b01f4c
teprinciple <teprinciple@foxmail.com>
2019-06-10 22:39:04 +0800

    整理结构

---
+++ updateapputils/src/main/java/update/DownloadAppUtils.kt
@@ -0,0 +1,94 @@
+package update
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Environment
+import android.util.Log
+
+import com.liulishuo.filedownloader.BaseDownloadTask
+import com.liulishuo.filedownloader.FileDownloadLargeFileListener
+import com.liulishuo.filedownloader.FileDownloader
+import extension.TAG
+
+import java.io.File
+
+
+/**
+ * Created by Teprinciple on 2016/12/13.
+ */
+internal object DownloadAppUtils {
+
+
+    var downloadUpdateApkFilePath: String = "" //下载更新Apk 文件路径
+
+    /**
+     * 通过浏览器下载APK包
+     */
+    fun downloadForWebView(context: Context, url: String) {
+        val uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(intent)
+    }
+
+    /**
+     * App 下载APK包 并
+     */
+    fun download(context: Context, url: String, serverVersionName: String) {
+
+        val packageName = context.packageName
+        var filePath: String? = null
+        if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {//外部存储卡
+            filePath = Environment.getExternalStorageDirectory().absolutePath
+        } else {
+            Log.i(TAG, "没有SD卡")
+            return
+        }
+
+        val apkLocalPath = filePath + File.separator + packageName + "_" + serverVersionName + ".apk"
+
+        downloadUpdateApkFilePath = apkLocalPath
+
+        FileDownloader.setup(context)
+
+        FileDownloader.getImpl().create(url)
+            .setPath(apkLocalPath)
+            .setListener(object : FileDownloadLargeFileListener() {
+                override fun pending(task: BaseDownloadTask, soFarBytes: Long, totalBytes: Long) {
+
+                }
+
+                override fun progress(task: BaseDownloadTask, soFarBytes: Long, totalBytes: Long) {
+                    send(context, (soFarBytes * 100.0 / totalBytes).toInt(), serverVersionName)
+                }
+
+                override fun paused(task: BaseDownloadTask, soFarBytes: Long, totalBytes: Long) {}
+
+                override fun completed(task: BaseDownloadTask) {
+                    send(context, 100, serverVersionName)
+                }
+
+                override fun error(task: BaseDownloadTask, e: Throwable) {
+                    //Toast.makeText(context, "下载出错", Toast.LENGTH_SHORT).show();
+
+                    // todo 将错误回调给调用着
+                }
+
+                override fun warn(task: BaseDownloadTask) {}
+            }).start()
+    }
+
+
+    /**
+     * 发送通知进度
+     */
+    private fun send(context: Context, progress: Int, serverVersionName: String) {
+
+        // todo 这里需要优化
+        val intent = Intent("teprinciple.update")
+        intent.putExtra("progress", progress)
+        intent.putExtra("title", serverVersionName)
+        context.sendBroadcast(intent)
+    }
+}
---
+++ updateapputils/src/main/java/util/Utils.kt
@@ -0,0 +1,68 @@
+package util
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.Uri
+import android.os.Build
+import android.support.v4.content.FileProvider
+import update.DownloadAppUtils
+import java.io.File
+
+/**
+ * desc: Utils
+ * author: teprinciple on 2019-06-03.
+ */
+internal object Utils {
+
+
+    /**
+     * 跳转安装
+     */
+    fun installApk(context: Context,apkPath: String) {
+
+        val i = Intent(Intent.ACTION_VIEW)
+        val apkFile = File(apkPath)
+
+        // android 7.0 fileprovider 适配
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            i.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            val contentUri = FileProvider.getUriForFile(
+                context, context.packageName + ".fileprovider", apkFile)
+            i.setDataAndType(contentUri, "application/vnd.android.package-archive")
+        } else {
+            i.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive")
+        }
+
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(i)
+    }
+
+    /**
+     * 检测wifi是否连接
+     */
+    fun isWifiConnected(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+        if (cm != null) {
+            val networkInfo = cm.activeNetworkInfo
+            if (networkInfo != null && networkInfo.type == ConnectivityManager.TYPE_WIFI) {
+                return true
+            }
+        }
+        return false
+    }
+
+    /**
+     * 获取apk的版本号 currentVersionCode
+     */
+    fun getAPPLocalVersion(ctx: Context): PackageInfo? {
+        val manager = ctx.packageManager
+        return try {
+            manager.getPackageInfo(ctx.packageName, 0)
+        } catch (e: PackageManager.NameNotFoundException) {
+            null
+        }
+    }
+}
